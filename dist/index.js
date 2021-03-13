e65308619efde0b79ed7f67339f9fc14c14bfbeb
Karthik-Nayak98 <karunayak63@gmail.com>
2021-03-13 15:34:04 +0530

    update index.js file

--- dist/index.js
+++ dist/index.js
@@ -33,7 +33,7 @@ const run = async () => {
        } else if (event === 'issues') {
          message = issue_message;
 
-         let labels =  {name: 'proposal',
+         let label =  {name: 'proposal',
                       description: 'New changes or updates proposed',
                       color: 'BFD4F2', // this is a green color
                     }
@@ -48,7 +48,7 @@ const run = async () => {
            issue_number: context.issue.number,
            owner: context.repo.owner,
            repo: context.repo.repo,
-           labels: "name: 'proposal', description: 'New changes or updates proposed',color: 'BFD4F2'"
+           labels: [label],
          });
        }
 
